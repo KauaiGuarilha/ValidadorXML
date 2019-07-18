b2cc45314abc8bfce3006e3f35f6fd7278b987f5
Kauai Guarilha Freitas <kauai.guarilha@hotmail.com>
2019-07-17 21:29:14 -0300

    Update UnfeAutorizada

--- View/UnfeAutorizada.pas
+++ View/UnfeAutorizada.pas
@@ -136,14 +136,17 @@ type
   private
     umfrmProd_NFe_Autorizada : TfrmProd_NFe_Autorizada;
 
-    nodeInfNfe_ide, nodeInfNfe_emit, nodeInfNfe_enderEmit, nodeInfNfe_dest, nodeInfNfe_enderDest: IXMLNode;
-
     FIDE_NFE_Doc : TIDE_NFE_Doc;
     FEMIT_NFE_Doc : TEMIT_NFE_Doc;
     FENDEREMIT_NFE_Doc : TENDEREMIT_NFE_Doc;
     FDEST_NFE_Doc : TDEST_NFE_Doc;
     FENDERDEST_NFE_Doc : TENDERDEST_NFE_Doc;
 
+    procedure ide_nfe_doc;
+    procedure emit_nfe_doc;
+    procedure enderEmit_nfe_doc;
+    procedure dest_nfe_doc;
+    procedure enderDest_nfe_doc;
   public
 
   end;
@@ -157,134 +160,151 @@ implementation
 
 procedure TfrmNFe_Autorizada.btnOkClick(Sender: TObject);
 begin
-     try
-       OpenDialog1.Execute;
+  try
+    OpenDialog1.Execute;
+    XMLDocument1.LoadFromFile(OpenDialog1.FileName);
 
-       XMLDocument1.LoadFromFile(OpenDialog1.FileName);
-
-       nodeInfNfe_ide := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('ide');
-       nodeInfNfe_emit := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('emit');
-       nodeInfNfe_enderEmit := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('emit').ChildNodes.FindNode('enderEmit');
-       nodeInfNfe_dest := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('dest');
-       nodeInfNfe_enderDest := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('dest').ChildNodes.FindNode('enderDest');
-
-       {ide}
-
-       FIDE_NFE_Doc.NodeIde := nodeInfNfe_ide;
-       FIDE_NFE_Doc.PreencherIDE;
-
-       self.edtUF.Text := FIDE_NFE_Doc.TcUf;
-       self.edtCnfe.Text := FIDE_NFE_Doc.TcNF;
-       self.edtNatOp.Text := FIDE_NFE_Doc.TNatOp;
-       self.edtMod.Text := FIDE_NFE_Doc.TMood;
-       self.edtSerie.Text := FIDE_NFE_Doc.TSerie;
-       self.edtNNF.Text := FIDE_NFE_Doc.TNNF;
-       self.edtDhEmi.Text := FIDE_NFE_Doc.TDhEmi;
-       self.edtDhSaiEnt.Text := FIDE_NFE_Doc.TDhSaiEnt;
-       self.edtTpNF.Text := FIDE_NFE_Doc.TTpNF;
-       self.edtIdDest.Text := FIDE_NFE_Doc.TIdDest;
-       self.edtCMunFG.Text := FIDE_NFE_Doc.TCMunFG;
-       self.edtTpImp.Text := FIDE_NFE_Doc.TTpImp;
-       self.edtTpEmis.Text := FIDE_NFE_Doc.TTpEmis;
-       self.edtCDV.Text := FIDE_NFE_Doc.TCDV;
-       self.edtTpAmb.Text := FIDE_NFE_Doc.TTpAmb;
-       self.edtFinNFe.Text := FIDE_NFE_Doc.TFinNFe;
-       self.edtIndFinal.Text := FIDE_NFE_Doc.TIndFinal;
-       self.edtIndPres.Text := FIDE_NFE_Doc.TIndPres;
-       self.edtProcEmi.Text := FIDE_NFE_Doc.TProcEmi;
-       self.edtVerProc.Text := FIDE_NFE_Doc.TVerProc;
-
-       {emit}
-
-       FEMIT_NFE_Doc.NodeEmit := nodeInfNfe_emit;
-       FEMIT_NFE_Doc.PreencheEmit;
-
-       self.edtCnpj.Text := FEMIT_NFE_Doc.TCNPJ;
-       self.edtXNome.Text := FEMIT_NFE_Doc.TXNome;
-       self.edtXFant.Text := FEMIT_NFE_Doc.TXFant;
-       self.edtIE.Text := FEMIT_NFE_Doc.TIE;
-       self.edtIEST.Text := FEMIT_NFE_Doc.TIEST;
-       self.edtCRT.Text := FEMIT_NFE_Doc.TCRT;
-
-       {enderEmit}
-
-       FENDEREMIT_NFE_Doc.NodeEnderEmit := nodeInfNfe_enderEmit;
-       FENDEREMIT_NFE_Doc.PreencherEnderEmit;
-
-       self.edtXLgr.Text := FENDEREMIT_NFE_Doc.TXLgr;
-       self.edtNro.Text := FENDEREMIT_NFE_Doc.TNro;
-       self.edtXCpl.Text := FENDEREMIT_NFE_Doc.TXCpl; //Opcional
-       self.edtXBairro.Text := FENDEREMIT_NFE_Doc.TXBairro;
-       self.edtCMun.Text := FENDEREMIT_NFE_Doc.TCMun;
-       self.edtXMun.Text := FENDEREMIT_NFE_Doc.TXMun;
-       self.edtEUF.Text := FENDEREMIT_NFE_Doc.TEUF;
-       self.edtCEP.Text := FENDEREMIT_NFE_Doc.TCEP;
-       self.edtCPais.Text := FENDEREMIT_NFE_Doc.TCPais;
-       self.edtXPais.Text := FENDEREMIT_NFE_Doc.TXPais;
-       self.edtFone.Text := FENDEREMIT_NFE_Doc.TFone;
-
-       {dest}
-
-       FDEST_NFE_Doc.NodeDest := nodeInfNfe_dest;
-       FDEST_NFE_Doc.PreencherDest;
-
-       self.edtCPF.Text := FDEST_NFE_Doc.TCPF; //Opcional
-       //self.edtDCNPJ.Text := FDEST_NFE_Doc.TDCNPJ; //Opcional
-       self.edtDXNome.Text := FDEST_NFE_Doc.TDxNome;
-       self.edtIndIEDest.Text := FDEST_NFE_Doc.TIndIEDest;
-       //self.edtDIE.Text := FDEST_NFE_Doc.TDIE; //Opcional
-       //self.edtDEmail.Text := FDEST_NFE_Doc.TDEmail;
-
-       {enderDest}
-
-       FENDERDEST_NFE_Doc.NodeEnderDest := nodeInfNfe_enderDest;
-       FENDERDEST_NFE_Doc.PreencherEnderDest;
-
-       self.edtDxLgr.Text := FENDERDEST_NFE_Doc.TDxLgr;
-       self.edtDnro.Text := FENDERDEST_NFE_Doc.TDnro;
-       self.edtDxCpl.Text := FENDERDEST_NFE_Doc.TDxCpl; //Opcional
-       self.edtDxBairro.Text := FENDERDEST_NFE_Doc.TDxBairro;
-       self.edtDcMun.Text := FENDERDEST_NFE_Doc.TDcMun;
-       self.edtDxMun.Text := FENDERDEST_NFE_Doc.TDxMun;
-       self.edtDUF.Text := FENDERDEST_NFE_Doc.TDUF;
-       self.edtDCEP.Text := FENDERDEST_NFE_Doc.TDCEP;
-       self.edtDcPais.Text := FENDERDEST_NFE_Doc.TDcPais;
-       self.edtDxPais.Text := FENDERDEST_NFE_Doc.TDxPais;
-       self.edtDfone.Text := FENDERDEST_NFE_Doc.TDfone;
-
-     except on E: Exception do
-     end;
+    ide_nfe_doc;
+    emit_nfe_doc;
+    enderEmit_nfe_doc;
+    dest_nfe_doc;
+    enderDest_nfe_doc;
+  except on E: Exception do
+  end;
 
 end;
 
 procedure TfrmNFe_Autorizada.Button1Click(Sender: TObject);
 begin
-     ShowMessage('Em construção');
-     umfrmProd_NFe_Autorizada := TfrmProd_NFe_Autorizada.Create(nil);
-     try
-       umfrmProd_NFe_Autorizada.ShowModal;
-     finally
-        FreeAndNil(umfrmProd_NFe_Autorizada);
-     end;
-end;
-
-procedure TfrmNFe_Autorizada.FormClose(Sender: TObject;
-  var Action: TCloseAction);
-begin
-    FIDE_NFE_Doc.Destroy_TIDE_NFE_Doc;
-    FEMIT_NFE_Doc.Destroy_TEMIT_NFE_Doc;
-    FENDEREMIT_NFE_Doc.Destroy_TENDEREMIT_NFE_Doc;
-    FDEST_NFE_Doc.Destroy_TDEST_NFE_Doc;
-    FENDERDEST_NFE_Doc.Destroy_TENDERDEST_NFE_Doc;
+  ShowMessage('Em construção');
+  umfrmProd_NFe_Autorizada := TfrmProd_NFe_Autorizada.Create(nil);
+    try
+      umfrmProd_NFe_Autorizada.ShowModal;
+    finally
+      FreeAndNil(umfrmProd_NFe_Autorizada);
+    end;
 end;
 
 procedure TfrmNFe_Autorizada.FormCreate(Sender: TObject);
 begin
-    FIDE_NFE_Doc := TIDE_NFE_Doc.Create_TIDE_NFE_Doc;
-    FEMIT_NFE_Doc := TEMIT_NFE_Doc.Create_TEMIT_NFE_Doc;
-    FENDEREMIT_NFE_Doc := TENDEREMIT_NFE_Doc.Create_TENDEREMIT_NFE_Doc;
-    FDEST_NFE_Doc := TDEST_NFE_Doc.Create_TDEST_NFE_Doc;
-    FENDERDEST_NFE_Doc := TENDERDEST_NFE_Doc.Create_TENDERDEST_NFE_Doc;
+  FIDE_NFE_Doc := TIDE_NFE_Doc.Create_TIDE_NFE_Doc;
+  FEMIT_NFE_Doc := TEMIT_NFE_Doc.Create_TEMIT_NFE_Doc;
+  FENDEREMIT_NFE_Doc := TENDEREMIT_NFE_Doc.Create_TENDEREMIT_NFE_Doc;
+  FDEST_NFE_Doc := TDEST_NFE_Doc.Create_TDEST_NFE_Doc;
+  FENDERDEST_NFE_Doc := TENDERDEST_NFE_Doc.Create_TENDERDEST_NFE_Doc;
+end;
+
+procedure TfrmNFe_Autorizada.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  FIDE_NFE_Doc.Destroy_TIDE_NFE_Doc;
+  FEMIT_NFE_Doc.Destroy_TEMIT_NFE_Doc;
+  FENDEREMIT_NFE_Doc.Destroy_TENDEREMIT_NFE_Doc;
+  FDEST_NFE_Doc.Destroy_TDEST_NFE_Doc;
+  FENDERDEST_NFE_Doc.Destroy_TENDERDEST_NFE_Doc;
+end;
+
+procedure TfrmNFe_Autorizada.ide_nfe_doc;
+var nodeInfNfe_ide : IXMLNode;
+begin
+  nodeInfNfe_ide := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('ide');
+
+  FIDE_NFE_Doc.NodeIde := nodeInfNfe_ide;
+  FIDE_NFE_Doc.PreencherIDE;
+
+  self.edtUF.Text := FIDE_NFE_Doc.TcUf;
+  self.edtCnfe.Text := FIDE_NFE_Doc.TcNF;
+  self.edtNatOp.Text := FIDE_NFE_Doc.TNatOp;
+  self.edtMod.Text := FIDE_NFE_Doc.TMood;
+  self.edtSerie.Text := FIDE_NFE_Doc.TSerie;
+  self.edtNNF.Text := FIDE_NFE_Doc.TNNF;
+  self.edtDhEmi.Text := FIDE_NFE_Doc.TDhEmi;
+  self.edtDhSaiEnt.Text := FIDE_NFE_Doc.TDhSaiEnt;
+  self.edtTpNF.Text := FIDE_NFE_Doc.TTpNF;
+  self.edtIdDest.Text := FIDE_NFE_Doc.TIdDest;
+  self.edtCMunFG.Text := FIDE_NFE_Doc.TCMunFG;
+  self.edtTpImp.Text := FIDE_NFE_Doc.TTpImp;
+  self.edtTpEmis.Text := FIDE_NFE_Doc.TTpEmis;
+  self.edtCDV.Text := FIDE_NFE_Doc.TCDV;
+  self.edtTpAmb.Text := FIDE_NFE_Doc.TTpAmb;
+  self.edtFinNFe.Text := FIDE_NFE_Doc.TFinNFe;
+  self.edtIndFinal.Text := FIDE_NFE_Doc.TIndFinal;
+  self.edtIndPres.Text := FIDE_NFE_Doc.TIndPres;
+  self.edtProcEmi.Text := FIDE_NFE_Doc.TProcEmi;
+  self.edtVerProc.Text := FIDE_NFE_Doc.TVerProc;
+end;
+
+procedure TfrmNFe_Autorizada.emit_nfe_doc;
+var nodeInfNfe_emit : IXMLNode;
+begin
+  nodeInfNfe_emit := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('emit');
+
+  FEMIT_NFE_Doc.NodeEmit := nodeInfNfe_emit;
+  FEMIT_NFE_Doc.PreencheEmit;
+
+  self.edtCnpj.Text := FEMIT_NFE_Doc.TCNPJ;
+  self.edtXNome.Text := FEMIT_NFE_Doc.TXNome;
+  self.edtXFant.Text := FEMIT_NFE_Doc.TXFant;
+  self.edtIE.Text := FEMIT_NFE_Doc.TIE;
+  self.edtIEST.Text := FEMIT_NFE_Doc.TIEST;
+  self.edtCRT.Text := FEMIT_NFE_Doc.TCRT;
+end;
+
+procedure TfrmNFe_Autorizada.enderEmit_nfe_doc;
+var nodeInfNfe_enderEmit : IXMLNode;
+begin
+  nodeInfNfe_enderEmit := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('emit').ChildNodes.FindNode('enderEmit');
+
+  FENDEREMIT_NFE_Doc.NodeEnderEmit := nodeInfNfe_enderEmit;
+  FENDEREMIT_NFE_Doc.PreencherEnderEmit;
+
+  self.edtXLgr.Text := FENDEREMIT_NFE_Doc.TXLgr;
+  self.edtNro.Text := FENDEREMIT_NFE_Doc.TNro;
+  self.edtXCpl.Text := FENDEREMIT_NFE_Doc.TXCpl; //Opcional
+  self.edtXBairro.Text := FENDEREMIT_NFE_Doc.TXBairro;
+  self.edtCMun.Text := FENDEREMIT_NFE_Doc.TCMun;
+  self.edtXMun.Text := FENDEREMIT_NFE_Doc.TXMun;
+  self.edtEUF.Text := FENDEREMIT_NFE_Doc.TEUF;
+  self.edtCEP.Text := FENDEREMIT_NFE_Doc.TCEP;
+  self.edtCPais.Text := FENDEREMIT_NFE_Doc.TCPais;
+  self.edtXPais.Text := FENDEREMIT_NFE_Doc.TXPais;
+  self.edtFone.Text := FENDEREMIT_NFE_Doc.TFone;
+end;
+
+procedure TfrmNFe_Autorizada.dest_nfe_doc;
+var nodeInfNfe_dest : IXMLNode;
+begin
+  nodeInfNfe_dest := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('dest');
+
+  FDEST_NFE_Doc.NodeDest := nodeInfNfe_dest;
+  FDEST_NFE_Doc.PreencherDest;
+
+  self.edtCPF.Text := FDEST_NFE_Doc.TCPF; //Opcional
+  //self.edtDCNPJ.Text := FDEST_NFE_Doc.TDCNPJ; //Opcional
+  self.edtDXNome.Text := FDEST_NFE_Doc.TDxNome;
+  self.edtIndIEDest.Text := FDEST_NFE_Doc.TIndIEDest;
+  //self.edtDIE.Text := FDEST_NFE_Doc.TDIE; //Opcional
+  //self.edtDEmail.Text := FDEST_NFE_Doc.TDEmail;
+end;
+
+procedure TfrmNFe_Autorizada.enderDest_nfe_doc;
+var nodeInfNfe_enderDest: IXMLNode;
+begin
+  nodeInfNfe_enderDest := XMLDocument1.ChildNodes.FindNode('nfeProc').ChildNodes.FindNode('NFe').ChildNodes.FindNode('infNFe').ChildNodes.FindNode('dest').ChildNodes.FindNode('enderDest');
+
+  FENDERDEST_NFE_Doc.NodeEnderDest := nodeInfNfe_enderDest;
+  FENDERDEST_NFE_Doc.PreencherEnderDest;
+
+  self.edtDxLgr.Text := FENDERDEST_NFE_Doc.TDxLgr;
+  self.edtDnro.Text := FENDERDEST_NFE_Doc.TDnro;
+  self.edtDxCpl.Text := FENDERDEST_NFE_Doc.TDxCpl; //Opcional
+  self.edtDxBairro.Text := FENDERDEST_NFE_Doc.TDxBairro;
+  self.edtDcMun.Text := FENDERDEST_NFE_Doc.TDcMun;
+  self.edtDxMun.Text := FENDERDEST_NFE_Doc.TDxMun;
+  self.edtDUF.Text := FENDERDEST_NFE_Doc.TDUF;
+  self.edtDCEP.Text := FENDERDEST_NFE_Doc.TDCEP;
+  self.edtDcPais.Text := FENDERDEST_NFE_Doc.TDcPais;
+  self.edtDxPais.Text := FENDERDEST_NFE_Doc.TDxPais;
+  self.edtDfone.Text := FENDERDEST_NFE_Doc.TDfone;
 end;
 
 procedure TfrmNFe_Autorizada.Timer1Timer(Sender: TObject);
