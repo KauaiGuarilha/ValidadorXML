26458799c9ca54406f2b3abf56cd2b53d499aef6
Kauai Guarilha Freitas <kauai.guarilha@hotmail.com>
2019-08-14 00:04:04 -0300

    Update UfrmProd_NFe_Documento.pas

--- View/UfrmProd_NFe_Documento.pas
+++ View/UfrmProd_NFe_Documento.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Xml.xmldom, Xml.XMLIntf,
-  Xml.XMLDoc, Vcl.ExtCtrls, Vcl.Grids, Vcl.DBGrids, Data.DB, Datasnap.DBClient, DET_PROD_ITEM;
+  Xml.XMLDoc, Vcl.ExtCtrls, Vcl.Grids, Vcl.DBGrids, Data.DB, Datasnap.DBClient;
 
 type
   TfrmProd_NFe_Documento = class(TForm)
